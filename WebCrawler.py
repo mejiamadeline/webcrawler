3d22d9d94337058837762e6ea3a59311eeb6e95e
semava <68350079+semava@users.noreply.github.com>
2022-02-21 15:31:06 -0800

    I was trying to find a way to word count for all languages
    -there are still issues with it
    -counts numbers
    -spanish counter broken u.u

--- WebCrawler.py
+++ WebCrawler.py
@@ -1,90 +1,42 @@
+# -*- coding:utf-8 -*-
 import os
 import requests
-import nltk
 from langdetect import detect
 from bs4 import BeautifulSoup
-from konlpy.tag import Okt
 from collections import Counter
-from string import punctuation
-from urllib.parse import urljoin
 import re
-from urllib.request import urlopen
 
-#url = "https://www.cau.ac.kr"
-#url = "https://www.ipn.mx/"
-url = "https://www.devry.edu/"
+url = "https://www.cau.ac.kr/"
+#url = "https://www.jornada.com.mx/"
+#url = "https://www.devry.edu/"
 
 response = requests.get(url, verify = False)
 html = response.text
-soup = BeautifulSoup(html, 'html.parser')   # Request html from the url and use Beautifulsoup to parse
+soup = BeautifulSoup(html, 'lxml')   # Request html from the url and use Beautifulsoup, lxml is more robust parser than html.parse and get rid of html tags for lang
 
 for script in soup(["script", "style"]):    # Get rid of javascript and css from the html
     script.decompose()
 
-outlinks = []                          
-for append_url in soup.find_all('a', href= True):
-    append_url = append_url.get('href')
-    append_url = urljoin(url, append_url)
-    outlinks.append(append_url)                      # I was able to extract the links inside outlinks[], but can't figure out how to crawl each one of them.
+soup_string = str(soup.text)                 # Convert the html into string
+soup_string = soup_string.replace('\n', ' ')
+soup_string = re.sub(" \d+", '', soup_string)
 
-def getDisallowed():
-    robotsDUrl = "https://www.devry.edu/robots.txt"
-    html2 = urlopen(robotsDUrl).read()
-    soup2 = BeautifulSoup(html2, 'html.parser')
+print(soup_string)
+print("The URL is in the following language: ", detect(soup.text)) #gets text context of html file
 
-    for script in soup(["script", "style"]):
-        script.decompse()
-    
-    robot = (soup2.get_text())
-    robots = re.split( ':|\n', robot)
-    dontNeed = ['Disallow', ' *','# FOR ALL OTHER USER AGENTS','User-agent', '',
-    '# BLOCKED AGENTS', ' Zing-BottaBot*', ' /', '# SITEMAP LOCATION', 'Sitemap', 
-    ' https', '//www.devry.edu/sitemap.xml']
-    for word in list(robots):
-        if word in dontNeed:
-            robots.remove(word)
+count_words = Counter(word for word in soup_string.strip().split(" ") if len(word) > 1 or word is ('a' or 'I'))      
+words_list = count_words.most_common(100)   # Count the 100 most frequent words
+print(words_list)
 
-    print(robots)
-    #print(type(rs))
-    #print(type(soup2))
-    #print(soup2.prettify)
-
-# I made an if else statement that checks the language of the html.
-# If it is not Korean, then count top 100 most frequent words without using the konply library
-# If it is in Korean, then use the konlpy library and count top 100 most frequent words.
-text = soup.get_text()
-if detect(text) != "ko":                            
-    text = (''.join(s.findAll(text=True))for s in soup.findAll('p'))
-    count_words = Counter((a.rstrip(punctuation).lower() for b in text for a in b.split()))
-    words_list = (count_words.most_common(100))
-else:
-    soup_string = str(soup)                 # Convert the html into string
-    okt = Okt()                             
-    korean_noun = okt.nouns(soup_string)    # From the converted string, extract only Korean nouns
-    words_only = str(korean_noun)           # Convert the nouns into string
-    print("The URL is in the following language: ", detect(words_only))
-    count_words = Counter(korean_noun)      
-    words_list = count_words.most_common(100)   # Count the 100 most frequent words
-
-filename_words = "C:/users/jjung/repository/words.csv"
-filename_html = "C:/users/jjung/repository/html_output.txt"
-
-os.makedirs(os.path.dirname(filename_html), exist_ok=True)
-with open(filename_html, "w") as file:
-    file.write(str(soup))
+filename_words = "C:/users/semav/repository/words.csv"
+filename_html = "C:/users/semav/repository/html_output.txt"
 
 os.makedirs(os.path.dirname(filename_words), exist_ok=True)     # Create a folder named repository and save files
 with open(filename_words, 'w', encoding = "utf-8-sig") as file:
     for noun, number in words_list:
         file.write( "{}, {}\n".format(noun, number) )
 
-def main():
-    getDisallowed()
-if __name__ == "__main__":
-    main()
-
-
-
-
-
+os.makedirs(os.path.dirname(filename_html), exist_ok=True)
+with open(filename_html, "w") as file:
+    file.write(str(soup))
 
