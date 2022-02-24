c7e291950f728c9e264b2c65e0bdf5dbef8ea106
Maddie <mejiamadeline23@gmail.com>
2022-02-23 18:02:49 -0800

    updated for crawling and printing html

--- WebCrawler_test_with_functions.py
+++ WebCrawler_test_with_functions.py
@@ -1,66 +1,122 @@
+from itertools import count
 import os
 import requests
-import re
+import nltk
 from langdetect import detect
 from bs4 import BeautifulSoup
 from konlpy.tag import Okt
 from collections import Counter
 from string import punctuation
 from urllib.parse import urljoin
+import re
 from urllib.request import urlopen
+import urllib.robotparser 
 
-def crawl_domain():
+def main():
     options = ["https://www.cau.ac.kr","https://www.ipn.mx/","https://www.devry.edu/"]
     print("Which url would you like to crawl")
     count=0
 
     for each in options: 
         print(count, " ",each)
-        count = count + 1
-    
+        count = count+1
     num = int(input())
-    file_num = num + 1
-    url = options[num]
-    file_path = input("Please enter the file path that you would like to use: ")
-    response = requests.get(url, verify = False)
+    file_num = num+1
+    mainURL = options[num]
+    file_path =input("Please enter the file path that you would like to use: ")
+    crawled = []
+    report = []
+    soup, outlinks, crawledOut  = crawl_domain(mainURL,crawled)
+    crawled = crawledOut
+
+    counter = 0
+    outlinksN = 500
+    urlList = []
+    outlinkCount = []
+    allSoup = []
+
+    words_list = check_language(soup)
+    save_files(file_path,file_num,words_list,soup,report)
+
+    for links in outlinks:
+        if counter > 5:
+            break
+        thisCounter, url, thisSoup, validOutlinks = goCrawl(mainURL, links, crawled)
+        allSoup.append(thisSoup)
+        
+        crawled.append(links)
+        urlList.append(url)
+        outlinkCount.append(thisCounter)
+        counter += 1
+    report.append(urlList)
+    report.append(outlinkCount)
+    save_htmls(file_path, file_num, allSoup)
+    print(words_list)
+    print(crawled)
+    print(report)
+
+def crawl_domain(mainURL,crawled):
+    
+    response = requests.get(mainURL, verify = False)
     html = response.text
     soup = BeautifulSoup(html, 'lxml')   # Request html from the url and use Beautifulsoup to parse
 
     for script in soup(["script", "style"]):    # Get rid of javascript and css from the html
         script.decompose()
 
-    outlinks = []                          
+    outlinks = []           
+    outlinksCounter = 0               
     for append_url in soup.find_all('a', href= True):
         append_url = append_url.get('href')
-        append_url = urljoin(url, append_url)
+        append_url = urljoin(mainURL, append_url)
         outlinks.append(append_url)  
+        outlinksCounter += 1
+    crawled.append(mainURL)
 
-    return file_num, file_path, soup, url
+    return soup, outlinks, crawled
 
-def get_disallowed(soup, url):
-    #robotsDUrl = "https://www.devry.edu/robots.txt"
-    #robotsDUrl = url + "/robots.txt"
-    response = requests.get(url, verify = False)
-    html2 = response.text
-    soup2 = BeautifulSoup(html2, 'lxml')
+def getDisallowed(mainURL, pageVisting):
+    robots = urllib.robotparser.RobotFileParser()
+    robotsURL = mainURL + "/robots.txt"
+    robots.set_url(robotsURL)
+    robots.read()
+    check = robots.can_fetch("*", pageVisting)
+    return check 
 
-    for script in soup(["script", "style"]):
-        script.decompse()
+def goCrawl(mainURL, url, crawled):
+    page = requests.get(url, verify = False)
+    thisHTML = page.text
+    thisSoup = BeautifulSoup(thisHTML,'lxml')
+
+    for script in thisSoup(["script", "style"]):
+        script.decompose()
+
+    thisOutlinks = []
+    thisCounter = 0
+    for append_url in thisSoup.find_all('a', href= True):
+        append_url = append_url.get('href')
+        append_url = urljoin(url, append_url)
+        thisOutlinks.append(append_url)  
+        thisCounter += 1
     
-    robot = (soup2.get_text())
-    robots = re.split( ':|\n', robot)
-    dont_need = ['Disallow', ' *','# FOR ALL OTHER USER AGENTS','User-agent', '',
-    '# BLOCKED AGENTS', ' Zing-BottaBot*', ' /', '# SITEMAP LOCATION', 'Sitemap', 
-    ' https', '//www.devry.edu/sitemap.xml']
-    for word in list(robots):
-        if word in dont_need:
-            robots.remove(word)
+    validOutlinks = []
+    for links in thisOutlinks:
+        if links in validOutlinks:
+            if (links in crawled):
+                pass
+            else:
+                if getDisallowed(mainURL, links) == True:
+                    if "https://" in links:
+                        validOutlinks.append(links)
+                    else: pass
+                else:
+                    pass
 
-    #print(robots)
-    #print(type(rs))
-    #print(type(soup2))
-    #print(soup2.prettify)
+    return thisCounter, url, thisSoup, validOutlinks
 
+# I made an if else statement that checks the language of the html.
+# If it is not Korean, then count top 100 most frequent words without using the konply library
+# If it is in Korean, then use the konlpy library and count top 100 most frequent words.
 def check_language(soup):
     
     text = soup.get_text()
@@ -79,9 +135,10 @@ def check_language(soup):
     print("The URL is in the following language: ", detect(soup.text)) 
     return words_list
 
-def save_files(file_path, file_num, words_list, soup):
+def save_files(file_path, file_num, words_list, soup, report):
     filename_words = f"{file_path}/repository/words{file_num}.csv"
     filename_html = f"{file_path}/repository/html_output{file_num}.html"
+    filename_report = f"{file_path}/repository/report{file_num}.csv"
 
     os.makedirs(os.path.dirname(filename_html), exist_ok=True)
     with open(filename_html, "w") as file:
@@ -91,12 +148,33 @@ def save_files(file_path, file_num, words_list, soup):
     with open(filename_words, 'w', encoding = "utf-8-sig") as file:
         for noun, number in words_list:
             file.write("{}, {}\n".format(noun, number))
-
-def main():
-    file_num, file_path, soup, url = crawl_domain()
-    get_disallowed(soup, url)
-    words_list = check_language(soup)
-    save_files(file_path, file_num, words_list, soup)
     
+    os.makedirs(os.path.dirname(filename_report),exist_ok=True)
+    with open(filename_report, "w") as file2:
+        for url, number in report:
+            file2.write("{}, {}\n".format(url, number))
+
+def save_htmls(file_path, file_num, thisSoup):
+    out =1 
+    for soup in thisSoup:
+        filename_outhtml = f"{file_path}/repository/html_output{file_num}({out}).html"
+        os.makedirs(os.path.dirname(filename_outhtml), exist_ok=True)
+        with open(filename_outhtml, "w") as file:
+            file.write(str(thisSoup))
+        out += 1
+        print(thisSoup)
+        
+  
+
+# This for loop iterates through all the links in the outlinks list and requests the html, but currently missing a feature to
+# save the visited link's html to a new file, e.g. html_output(2).html for the second visited link, html_outout(3).html for the third link, ...
+
+    '''for item in outlinks:
+    outlink_response = requests.get(item, verify = False)
+    outlink_html = outlink_response.text
+    outlink_soup = BeautifulSoup(outlink_html, 'html.parser')
+    for script in outlink_soup(['script', 'style']):
+        script.decompose()'''
+
 if __name__ == "__main__":
     main()
